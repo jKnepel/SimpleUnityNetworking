9ee94fbc434df372ebd32e51306cd47adc8d31cb
jKnepel <knepel.julian@tutanota.com>
2023-11-12 22:37:26 +0100

    chore: update descriptions

--- Assets/SimpleUnityNetworking/Runtime/Scripts/Serialisation/SerialiserConfiguration.cs
+++ Assets/SimpleUnityNetworking/Runtime/Scripts/Serialisation/SerialiserConfiguration.cs
@@ -6,22 +6,40 @@ namespace jKnepel.SimpleUnityNetworking.Serialisation
     [Serializable]
     public class SerialiserConfiguration
     {
+		/// <summary>
+		/// Whether floats should be automatically compressed by the serialiser.
+		/// </summary>
+		public bool CompressFloats { get => _compressFloats; set => _compressFloats = value; }
         [SerializeField] private bool _compressFloats = false;
-        public bool CompressFloats { get => _compressFloats; set => _compressFloats = value; }
 
-        [SerializeField] private float _floatMinValue = -1000;
+        /// <summary>
+        /// The minimum value defining the range in which the compressed float can be saved.
+        /// </summary>
         public float FloatMinValue { get => _floatMinValue; set => _floatMinValue = value; }
+        [SerializeField] private float _floatMinValue = -1000;
 
-        [SerializeField] private float _floatMaxValue = 1000;
+        /// <summary>
+        /// The maximum value defining the range in which the compressed float can be saved.
+        /// </summary>
         public float FloatMaxValue { get => _floatMaxValue; set => _floatMaxValue = value; }
+        [SerializeField] private float _floatMaxValue = 1000;
 
-        [SerializeField] private float _floatResolution = 0.001f;
+        /// <summary>
+        /// The floating point resolution in which the float is serialised.
+        /// </summary>
         public float FloatResolution { get => _floatResolution; set => _floatResolution = value; }
+        [SerializeField] private float _floatResolution = 0.001f;
 
-        [SerializeField] private bool _compressQuaternions = false;
+        /// <summary>
+        /// Whether Quaternions should be automatically compressed by the serialiser.
+        /// </summary>
         public bool CompressQuaternions { get => _compressQuaternions; set => _compressQuaternions = value; }
+        [SerializeField] private bool _compressQuaternions = false;
 
-        [SerializeField] private int _bitsPerComponent = 9;
+        /// <summary>
+        /// The number of bits used by each compressed Quaternion component.
+        /// </summary>
         public int BitsPerComponent { get => _bitsPerComponent; set => _bitsPerComponent = value; }
+        [SerializeField] private int _bitsPerComponent = 9;
     }
 }
