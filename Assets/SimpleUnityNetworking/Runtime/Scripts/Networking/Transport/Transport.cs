e34a7e41d6ec43342655da5e618d3aa88f07ecf2
jKnepel <knepel.julian@tutanota.com>
2024-03-01 19:42:25 +0100

    feat: added pipeline settings to transport

--- Assets/SimpleUnityNetworking/Runtime/Scripts/Networking/Transport/Transport.cs
+++ Assets/SimpleUnityNetworking/Runtime/Scripts/Networking/Transport/Transport.cs
@@ -23,6 +23,7 @@ namespace jKnepel.SimpleUnityNetworking.Transporting
         public abstract event Action<ELocalConnectionState> OnClientStateUpdated;
         public abstract event Action<int, ERemoteConnectionState> OnConnectionUpdated;
 
+        public abstract void SetTransportSettings(TransportSettings settings);
         public abstract void StartServer();
         public abstract void StopServer();
         public abstract void StartClient();
@@ -36,33 +37,64 @@ namespace jKnepel.SimpleUnityNetworking.Transporting
     }
     
     [Serializable]
-    public class ConnectionData
+    public class TransportSettings
     {
+        /// <summary>
+        /// The address to which the local client will attempt to connect with.
+        /// </summary>
         public string Address = "127.0.0.1";
+        /// <summary>
+        /// The port to which the local client will attempt to connect with or the server will bind to locally.
+        /// </summary>
         public ushort Port = 24856;
+        /// <summary>
+        /// Address to which the local server will be bound. If no address is provided, the IPv4 Loopback
+        /// address will be used instead.
+        /// </summary>
         public string ServerListenAddress = string.Empty;
-        
-        public static NetworkEndpoint ParseNetworkEndpoint(string ip, ushort port)
-        {
-            if (NetworkEndpoint.TryParse(ip, port, out var endpoint) ||
-                NetworkEndpoint.TryParse(ip, port, out endpoint, NetworkFamily.Ipv6)) return endpoint;
-
-            return endpoint;
-        }
-
-        public virtual NetworkEndpoint ParseServerListenEndpoint()
-        {
-            if (!string.IsNullOrEmpty(ServerListenAddress))
-            {
-                return ParseNetworkEndpoint(ServerListenAddress, Port);
-            }
-            
-            var endpoint = !string.IsNullOrEmpty(Address) &&
-                           ParseNetworkEndpoint(Address, Port).Family == NetworkFamily.Ipv6
-                ? NetworkEndpoint.LoopbackIpv6
-                : NetworkEndpoint.LoopbackIpv4;
-            return endpoint.WithPort(Port);
-        }
+        /// <summary>
+        /// Time between connection attempts.
+        /// </summary>
+        public int ConnectTimeoutMS = 1000;
+        /// <summary>
+        /// Maximum number of connection attempts to try. If no answer is received from the server
+        /// after this number of attempts, a disconnect event is generated for the connection.
+        /// </summary>
+        public int MaxConnectAttempts = 60;
+        /// <summary>
+        /// Inactivity timeout for a connection. If nothing is received on a connection for this
+        /// amount of time, it is disconnected. To prevent this from happening when the game session is simply
+        /// quiet, set <c>HeartbeatTimeoutMS</c> to a positive non-zero value.
+        /// </summary>
+        public int DisconnectTimeoutMS = 30000;
+        /// <summary>
+        /// Time after which if nothing from a peer is received, a heartbeat message will be sent
+        /// to keep the connection alive. Prevents the <c>DisconnectTimeoutMS</c> mechanism from
+        /// kicking when nothing happens on a connection. A value of 0 will disable heartbeats.
+        /// </summary>
+        public int HeartbeatTimeoutMS = 500;
+        /// <summary>
+        /// Maximum size that can be fragmented. Attempting to send a message larger than that will
+        /// result in the send operation failing. Maximum value is ~20MB for unreliable packets,
+        /// and ~88KB for reliable ones.
+        /// </summary>
+        public int PayloadCapacity = 4096;
+        /// <summary>
+        /// Maximum number in-flight packets per pipeline/connection combination. Default value
+        /// is 32 but can be increased to 64 at the cost of slightly larger packet headers.
+        /// </summary>
+        public int WindowSize = 32;
+        /// <summary>
+        /// Minimum amount of time to wait before a reliable packet is resent if it's not been
+        /// acknowledged.
+        /// </summary>
+        public int MinimumResendTime = 64;
+        /// <summary>
+        /// Maximum amount of time to wait before a reliable packet is resent if it's not been
+        /// acknowledged. That is, even with a high RTT the reliable pipeline will never wait
+        /// longer than this value to resend a packet.
+        /// </summary>
+        public int MaximumResendTime = 200;
     }
     
     public struct ServerReceivedData
